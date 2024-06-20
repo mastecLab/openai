840a242631cb4880040dc38bdc1070371d0d84b2
mastec <sebagtz@gmail.com>
2024-06-20 09:51:35 +0200

    Update convert_text_to_audio.py

--- video/convert_text_to_audio.py
+++ video/convert_text_to_audio.py
@@ -8,9 +8,7 @@ speech_file_path = Path(__file__).parent / "speech.mp3"
 response = openai.audio.speech.create(
 model="tts-1",
 voice="alloy",
- # input="Felicidades Julen"
 input= content 
-  
  # f = open("transcription1_2.txt", "w", encoding="utf-8")
 )
 response.stream_to_file(speech_file_path)
